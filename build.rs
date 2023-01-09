6b4ad4a7c446c1cd0eef554ffc54dbfeb3cd71d7
Sam Uong <samuong@gmail.com>
2023-01-09 11:34:49 +1100

    Move functionality into Octave

--- build.rs
+++ build.rs
@@ -2,5 +2,12 @@ fn main() {
     cxx_build::bridge("src/main.rs")
         .file("src/main.cc")
         .flag_if_supported("-std=c++14")
+        .flag_if_supported("-I/usr/include/octave-7.2.0")
         .compile("octave-language-server");
+
+    println!("cargo:rerun-if-changed=src/main.cc");
+    println!("cargo:rerun-if-changed=src/main.h");
+
+    println!("cargo:rustc-link-lib=octinterp");
+    println!("cargo:rustc-link-search=/usr/lib64/octave/7.2.0");
 }
--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,26 @@
 
 #include <iostream>
 
+#include <octave/interpreter.h>
 
 void cpp_main() {
-  std::cout << "Hello, world!" << std::endl;
+  octave::interpreter interp;
+  interp.initialize();
+  if (!interp.initialized()) {
+    std::cerr << "Octave interpreter initialization failed!" << std::endl;
+    return;
+  }
+  int status = interp.execute();
+  if (status != 0) {
+    std::cerr << "Creating embedded Octave interpreter failed!" << std::endl;
+    return;
+  }
+  int parse_status = 0;
+  std::string input = "disp('Hello, world!')";
+  octave_value output = interp.eval_string(input, true, parse_status);
+  if (parse_status != 0) {
+    std::cerr << "Parsing embedded Octave sources failed!" << std::endl;
+    return;
+  }
+  output.print(std::cout);
 }
