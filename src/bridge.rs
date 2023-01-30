df2f81fdc0adbb16cecc1e42d00b40da34e17424
Sam Uong <samuong@gmail.com>
2023-01-30 13:48:17 +1100

    Remove dependency on anyhow and unwrap errors (instead of propagating)
    Unwrapping gives better error messages (it also prints the file and line
    number, instead of just the exception message) and offers to give you a
    backtrace if you re-run wtih RUST_BACKTRACE=1.

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,5 +1,3 @@
-use anyhow::Result;
-
 #[cxx::bridge]
 pub mod ffi {
     unsafe extern "C++" {
@@ -20,23 +18,22 @@ mod tests {
 
     #[test]
     #[serial]
-    fn test_symbol_at() -> Result<()> {
-        ffi::init()?;
+    fn test_symbol_at() {
+        ffi::init().unwrap();
         ffi::analyse("msg = 'Hello, world!'\ndisp(msg)\n");
-        assert_eq!(ffi::symbol_at(0, 0)?, "msg");
-        assert_eq!(ffi::symbol_at(1, 3)?, "disp");
+        assert_eq!(ffi::symbol_at(0, 0).unwrap(), "msg");
+        assert_eq!(ffi::symbol_at(1, 3).unwrap(), "disp");
         assert!(ffi::symbol_at(1, 4).is_err());
-        assert_eq!(ffi::symbol_at(1, 5)?, "msg");
+        assert_eq!(ffi::symbol_at(1, 5).unwrap(), "msg");
         assert!(ffi::symbol_at(1, 10).is_err());
         assert!(ffi::symbol_at(2, 0).is_err());
         ffi::clear_indexes(); // TODO: always run this, even if we finish the test early
-        Ok(())
     }
 
     #[test]
     #[serial]
-    fn test_goto_def() -> Result<()> {
-        ffi::init()?;
+    fn test_goto_def() {
+        ffi::init().unwrap();
         ffi::analyse(
             r#"
 function sum = add (augend, addend)
@@ -46,11 +43,10 @@ f = @add;
 y = f (1, 2);
 "#,
         );
-        let symbol = ffi::symbol_at(3, 5)?;
+        let symbol = ffi::symbol_at(3, 5).unwrap();
         assert_eq!(symbol, "add");
-        let pos = ffi::definition(symbol.as_str())?;
+        let pos = ffi::definition(symbol.as_str()).unwrap();
         assert_eq!(pos, [0, 0]);
         ffi::clear_indexes(); // TODO: always run this, even if we finish the test early
-        Ok(())
     }
 }
