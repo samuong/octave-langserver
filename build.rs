3cc29b17f9c73ac7eae8847d6a3beafa3e09e9a8
Sam Uong <samuong@gmail.com>
2023-02-28 18:01:02 +1100

    Invoke mkoctfile to determine compiler/linker flags
    This will hopefully work better on systems that have include files and
    libraries installed in different paths from mine.

--- build.rs
+++ build.rs
@@ -1,9 +1,22 @@
+use std::io::BufRead;
+use std::process::Command;
+
 fn main() {
+    let output = Command::new("mkoctfile")
+        .args(["--dry-run", "--link-stand-alone", "src/tree_walker.cc"])
+        .output()
+        .unwrap();
+    let mut stdout = output.stdout.lines();
+    let compile_cmd = stdout.next().unwrap().unwrap();
+    let include_dirs = compile_cmd
+        .split_whitespace()
+        .filter_map(|s| s.strip_prefix("-I"));
+
     cxx_build::bridge("src/bridge.rs")
         .file("src/bridge.cc")
         .file("src/tree_walker.cc")
         .flag("-std=c++11")
-        .flag("-I/usr/include/octave-7.3.0")
+        .includes(include_dirs)
         .compile("octave-langserver");
 
     println!("cargo:rerun-if-changed=src/bridge.cc");
@@ -11,8 +24,13 @@ fn main() {
     println!("cargo:rerun-if-changed=src/tree_walker.cc");
     println!("cargo:rerun-if-changed=src/tree_walker.h");
 
-    println!("cargo:rustc-link-lib=octave");
-    println!("cargo:rustc-link-lib=octinterp");
-    println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-search=/usr/lib64/octave/7.3.0");
+    let link_cmd = stdout.next().unwrap().unwrap();
+    for link_arg in link_cmd.split_whitespace() {
+        if let Some(lib) = link_arg.strip_prefix("-l") {
+            println!("cargo:rustc-link-lib={lib}");
+        } else if let Some(lib_dir) = link_arg.strip_prefix("-L") {
+            println!("cargo:rustc-link-search={lib_dir}");
+        }
+    }
+    assert!(stdout.next().is_none());
 }
